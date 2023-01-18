1d114b72a8aae290f15f31234adb96a72c5d882c
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-18 13:44:45 +0000

    Added classes and methods to  retrieve spell names

---
+++ Week6_Async/DnD5eSpellfinder/DnD5eSpellfinderApp/Model.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DnD5eSpellfinderApp;
+
+
+public class SpellResult
+{
+    [JsonProperty("count")]
+    public int Count { get; set; }
+    public string next { get; set; }
+    public object previous { get; set; }
+    public Spell[] results { get; set; }
+    public override string ToString()
+    {
+        string outputString = "";
+        foreach (var spell in results)
+        {
+            outputString += spell.name + "\r\n";
+        }
+        return outputString;
+    }
+}
+
+public class Spell
+{
+    public string slug { get; set; }
+    public string name { get; set; }
+    public string desc { get; set; }
+    public string higher_level { get; set; }
+    public string page { get; set; }
+    public string range { get; set; }
+    public string components { get; set; }
+    public string material { get; set; }
+    public string ritual { get; set; }
+    public string duration { get; set; }
+    public string concentration { get; set; }
+    public string casting_time { get; set; }
+    public string level { get; set; }
+    public int level_int { get; set; }
+    public string school { get; set; }
+    public string dnd_class { get; set; }
+    public string archetype { get; set; }
+    public string circles { get; set; }
+    public string document__slug { get; set; }
+    public string document__title { get; set; }
+    public string document__license_url { get; set; }
+}
--- Week6_Async/DnD5eSpellfinder/DnD5eSpellfinderApp/Program.cs
+++ Week6_Async/DnD5eSpellfinder/DnD5eSpellfinderApp/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace DnD5eSpellfinderApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using RestSharp;
+using Newtonsoft.Json;
 
-        }
+namespace DnD5eSpellfinderApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(GetSpellFromName("fireball"));
+    }
+
+    public static SpellResult GetSpellFromName(string spellName)
+    {
+        var client = new RestClient("https://api.open5e.com/spells");
+        var request = new RestRequest($"/?{spellName}");
+        request.Timeout = -1;
+        request.AddHeader("Content-Type", "application/json");
+        RestResponse response = client.Execute(request);
+        return JsonConvert.DeserializeObject<SpellResult>(response.Content);
     }
 }
