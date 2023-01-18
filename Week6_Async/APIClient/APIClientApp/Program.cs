7b056c3cd780b44bb635fa78f7ddd6896f2c6dbb
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-18 12:22:22 +0000

    Added requests to the api.postcodes.io API

--- Week6_Async/APIClient/APIClientApp/Program.cs
+++ Week6_Async/APIClient/APIClientApp/Program.cs
@@ -1,9 +1,50 @@
-﻿namespace APIClientApp;
+﻿using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
 
-internal class Program
+namespace APIClientApp;
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        var postcode = GetSinglePostcode("EC2Y5AS");
+        Console.WriteLine(postcode.Postcode.admin_district);
 
+        var postcodes = GetMultiplePostcodes(new string[] { "PR3 0SG", "M45 6GN", "EX165BL" });
+        foreach (var response in postcodes.Responses)
+        {
+            Console.WriteLine(response.Postcode.admin_district);
+        }
+    }
+
+    public static BulkPostcodeResponse GetMultiplePostcodes(string[] postcodes)
+    {
+        var client = new RestClient("https://api.postcodes.io/postcodes");
+        var request = new RestRequest();
+        request.Method = Method.Post;
+        request.Timeout = -1;
+        request.AddHeader("Content-Type", "application/json");
+        request.AddJsonBody(new {Postcodes = postcodes });
+        RestResponse response = client.Execute(request);
+        return JsonConvert.DeserializeObject<BulkPostcodeResponse>(response.Content);
+    }
+
+    public static SinglePostcodeResponse GetSinglePostcode(string postcode)
+    {
+        var restClient = new RestClient("https://api.postcodes.io/");
+
+        var restRequest = new RestRequest();
+
+        //build the request
+        restRequest.Method = Method.Get;
+        restRequest.AddHeader("Content-Type", "application/json");
+        restRequest.Timeout = -1;
+        restRequest.Resource = $"postcodes/{postcode}";
+
+        var singlePostcodeResponse = restClient.Execute(restRequest);
+
+        return JsonConvert.DeserializeObject<SinglePostcodeResponse>(singlePostcodeResponse.Content);
     }
 }
