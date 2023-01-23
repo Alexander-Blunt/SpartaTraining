e2aa04e1f9f7f34da6d9d612e20dbed6f485d11a
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-23 12:35:00 +0000

    Added MoqStarter, contains an introduction to mocking

---
+++ Week6_Async/CustomerApp_MoqStarter/NorthwindTests/CustomerManagerShould.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Moq;
+using NorthwindBusiness;
+using NorthwindData;
+using NorthwindData.Services;
+using System.Data;
+
+namespace NorthwindTests
+{
+    public class CustomerManagerShould
+    {
+        private CustomerManager _sut;
+
+        [Test]
+        public void BeConstructable()
+        {
+            //Arrange
+            var dummyCustomerService = new Mock<ICustomerService>().Object;
+
+            //Act
+            _sut = new CustomerManager(dummyCustomerService); //null can also act as a dummy
+
+            //Assert
+            Assert.That(_sut, Is.InstanceOf<CustomerManager>());
+        }
+
+        [Test]
+        public void ReturnTrue_WhenUpdateIsCalled_WithValidId()
+        {
+            //Arrange
+            var mockCustomerService = new Mock<ICustomerService>();
+            var originalCustomer = new Customer() { CustomerId = "JSMITH" };
+
+            //We only need to implement the methods we are actually going to use.
+            mockCustomerService.Setup(cs => cs.GetCustomerById("JSMITH")).Returns(originalCustomer);
+
+            _sut = new CustomerManager(mockCustomerService.Object);
+
+            //Act
+            var result = _sut.Update("JSMITH", It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
