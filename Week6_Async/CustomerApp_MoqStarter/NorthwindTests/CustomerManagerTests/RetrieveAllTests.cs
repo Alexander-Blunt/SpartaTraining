2879829e68e32faca532f65971a95ba3b6e682e4
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-23 15:55:10 +0000

    Added Tests for CustomerManager.RetrieveAll

---
+++ Week6_Async/CustomerApp_MoqStarter/NorthwindTests/CustomerManagerTests/RetrieveAllTests.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using NorthwindBusiness;
+using NorthwindData.Services;
+using NorthwindData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindTests.CustomerManagerTests
+{
+    public class WhenRetrieveAllIsCalled
+    {
+        private CustomerManager _sut;
+
+        [Test]
+        public void AndCustomersExist_ReturnCustomerList()
+        {
+            //Arrange
+            var mockCustomerService = new Mock<ICustomerService>();
+            var CustomerList = new List<Customer>() {
+                new Customer(),
+                new Customer()
+            };
+            mockCustomerService.Setup(cs => cs.GetCustomerList()).Returns(CustomerList);
+            _sut = new CustomerManager(mockCustomerService.Object);
+
+            //Act
+            var result = _sut.RetrieveAll();
+
+            //Assert
+            Assert.That(result, Is.EqualTo(CustomerList));
+        }
+
+        [Test]
+        public void AndNoCustomersExist_ReturnEmptyList()
+        {
+            //Arrange
+            var mockCustomerService = new Mock<ICustomerService>();
+            var CustomerList = new List<Customer>() { };
+            mockCustomerService.Setup(cs => cs.GetCustomerList()).Returns(CustomerList);
+            _sut = new CustomerManager(mockCustomerService.Object);
+
+            //Act
+            var result = _sut.RetrieveAll();
+
+            //Assert
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
